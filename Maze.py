3cff9db9e86819c083ef92d656f308c6edc47209
이진영 <jakerhee321@gmail.com>
2022-08-25 14:13:40 +0900

    Destination

--- Maze.py
+++ Maze.py
@@ -1,17 +1,27 @@
 from tkinter import *
+from tkinter import messagebox
 
 def player_move(event):
-    if event.keysym == "Up":
+    global x, y
+    if event.keysym == "Up" and maze[y-1][x] != 1:
         canvas.move(player, 0, -70)
+        y -= 1
 
-    elif event.keysym == "Down":
+    elif event.keysym == "Down" and maze[y+1][x] != 1:
         canvas.move(player, 0, 70)
+        y += 1
 
-    elif event.keysym == "Left":
+    elif event.keysym == "Left" and maze[y][x-1] != 1:
         canvas.move(player, -70, 0)
+        x -= 1
 
-    elif event.keysym == "Right":
+    elif event.keysym == "Right" and maze[y][x+1] != 1:
         canvas.move(player, 70, 0)
+        x += 1
+
+    if maze[y][x] == 2:
+        messagebox.showinfo("Complete", "You have reached the destination.")
+
 
 window = Tk()
 window.title("Maze")
@@ -22,7 +32,7 @@ canvas.pack()
 
 maze = [
     [1,1,1,1,1,1,1,1,1,1],
-    [1,0,0,1,1,1,0,0,0,1],
+    [1,0,0,1,1,1,2,0,0,1],  # 2 is the destination
     [1,1,0,1,1,1,1,1,0,1],
     [1,0,0,1,0,0,0,1,0,1],
     [1,0,1,0,0,1,0,1,0,1],
@@ -30,11 +40,13 @@ maze = [
     [1,1,1,1,1,1,1,1,1,1]
 ]
 
-
 for y in range(7):
     for x in range(10):
         if maze[y][x] == 1:
             canvas.create_rectangle(x*70, y*70, (x+1)*70, (y+1)*70, fill="cyan", outline="white")
+
+x = 1
+y = 1
 
 player_image = PhotoImage(file="player.png")
 player = canvas.create_image(105,105,image=player_image)
