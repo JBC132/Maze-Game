c0c5cbe33cfeb98ba121a1b70859bea9919800d2
이진영 <jakerhee321@gmail.com>
2022-08-26 13:34:25 +0900

    Code for random mazes

--- Maze.py
+++ Maze.py
@@ -1,27 +1,28 @@
 from tkinter import *
 from tkinter import messagebox
+import random
 
 def player_move(event):
-    global x, y
-    if event.keysym == "Up" and maze[y-1][x] != 1:
+    global x, y, num
+
+    if event.keysym == "Up" and maze[num][y-1][x] != 1:
         canvas.move(player, 0, -70)
         y -= 1
-
-    elif event.keysym == "Down" and maze[y+1][x] != 1:
+    
+    elif event.keysym == "Down" and maze[num][y+1][x] != 1:
         canvas.move(player, 0, 70)
         y += 1
 
-    elif event.keysym == "Left" and maze[y][x-1] != 1:
+    elif event.keysym == "Left" and maze[num][y][x-1] != 1:
         canvas.move(player, -70, 0)
         x -= 1
 
-    elif event.keysym == "Right" and maze[y][x+1] != 1:
+    elif event.keysym == "Right" and maze[num][y][x+1] != 1:
         canvas.move(player, 70, 0)
         x += 1
 
-    if maze[y][x] == 2:
+    if maze[num][y][x] == 2:
         messagebox.showinfo("Complete", "You have reached the destination.")
-
 
 window = Tk()
 window.title("Maze")
@@ -29,8 +30,8 @@ window.title("Maze")
 canvas = Canvas(window, width=700, height=490, bg="light green")
 canvas.pack()
 
-
 maze = [
+    [
     [1,1,1,1,1,1,1,1,1,1],
     [1,0,0,1,1,1,2,0,0,1],  # 2 is the destination
     [1,1,0,1,1,1,1,1,0,1],
@@ -38,11 +39,24 @@ maze = [
     [1,0,1,0,0,1,0,1,0,1],
     [1,0,0,0,1,1,0,0,0,1],
     [1,1,1,1,1,1,1,1,1,1]
+    ],
+
+    [
+    [1,1,1,1,1,1,1,1,1,1],
+    [1,0,1,0,0,0,2,1,1,1],
+    [1,0,1,0,1,1,1,1,1,1],
+    [1,0,1,0,0,0,0,0,0,1],
+    [1,0,1,1,1,1,1,1,0,1],
+    [1,0,0,0,0,0,0,0,0,1],
+    [1,1,1,1,1,1,1,1,1,1]
+    ]
 ]
+
+num = random.randint(0, len(maze)-1)
 
 for y in range(7):
     for x in range(10):
-        if maze[y][x] == 1:
+        if maze[num][y][x] == 1:
             canvas.create_rectangle(x*70, y*70, (x+1)*70, (y+1)*70, fill="cyan", outline="white")
 
 x = 1
